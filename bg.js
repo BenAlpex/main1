5211ed389d7480264a036bc2d5d47d97136a8306
BenAlpex <168351779+BenAlpex@users.noreply.github.com>
2026-01-02 15:40:55 +0300

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,3 +1,10 @@
+const glow = document.getElementById("glow");
+
+document.addEventListener("mousemove", e => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
+
 document.addEventListener("mousemove", e => {
   document.querySelectorAll(".hex").forEach((h,i)=>{
     const x=(e.clientX/window.innerWidth-0.5)*20;
