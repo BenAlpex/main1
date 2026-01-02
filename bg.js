c6e698b3df537a8ca5045b682319a2ccc9b04237
BenAlpex <168351779+BenAlpex@users.noreply.github.com>
2026-01-02 15:37:53 +0300

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,8 +1,7 @@
 document.addEventListener("mousemove", e => {
-  document.querySelectorAll(".cube").forEach((cube, i) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 20;
-    const y = (e.clientY / window.innerHeight - 0.5) * 20;
-    cube.style.transform =
-      `rotateX(${y}deg) rotateY(${x}deg)`;
+  document.querySelectorAll(".hex").forEach((h,i)=>{
+    const x=(e.clientX/window.innerWidth-0.5)*20;
+    const y=(e.clientY/window.innerHeight-0.5)*20;
+    h.style.transform=`translate(${x}px,${y}px)`;
   });
 });
